4be89f98322875c012105012c208c6428416d952
Venkat3103 <venkatasubramanian2001@gmail.com>
2024-01-14 21:29:19 +0300

    optimally solved datalemur_data_science_skills

--- datalemur_data_science_skills.sql
+++ datalemur_data_science_skills.sql
@@ -1,8 +1,10 @@
-SELECT candidate_id 
-FROM (SELECT candidate_id, count(*) FROM candidates 
-        WHERE skill IN ('Python','Tableau','PostgreSQL') 
-        GROUP by candidate_id) as x 
-WHERE count=3 ORDER BY candidate_id;
+ 
+SELECT candidate_id
+FROM candidates
+WHERE skill in ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill)=3
+ORDER BY candidate_id;
 
 --https://datalemur.com/questions/matching-skills
 --LinkedIn - Easy
